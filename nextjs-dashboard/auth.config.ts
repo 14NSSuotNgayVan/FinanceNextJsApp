b3a389bb7df7ad0689cd4c2d3d54a8afb050e364
Dam Van Anh <anhmelol@gmail.com>
2024-10-29 10:47:20 +0700

    feat/NEXTJS: update auth

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -10,12 +10,15 @@ export const authConfig = {
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             if (isOnDashboard) {
                 if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
+                return false;
             } else if (isLoggedIn) {
                 return Response.redirect(new URL('/dashboard', nextUrl));
             }
             return true;
         },
+        async redirect({ url, baseUrl }) {
+            return url.startsWith(baseUrl) ? url : `${baseUrl}/dashboard`;
+        },
     },
-    providers: [], // Add providers with an empty array for now
+    providers: [],
 } satisfies NextAuthConfig;
